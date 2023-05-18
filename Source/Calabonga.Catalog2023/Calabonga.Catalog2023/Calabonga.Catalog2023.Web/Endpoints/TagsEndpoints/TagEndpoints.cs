0278d0a84a93a90360b3e586ee3f569c1f13a61f
Calabonga <dev@calabonga.net>
2023-05-18 16:12:21 +0700

    Video18 changes

---
+++ Source/Calabonga.Catalog2023/Calabonga.Catalog2023/Calabonga.Catalog2023.Web/Endpoints/TagsEndpoints/TagEndpoints.cs
@@ -0,0 +1,29 @@
+﻿using Calabonga.AspNetCore.AppDefinitions;
+using Calabonga.Catalog2023.Web.Application;
+using Calabonga.Catalog2023.Web.Endpoints.TagsEndpoints.Queries;
+using Calabonga.Catalog2023.Web.Endpoints.TagsEndpoints.ViewModels;
+using Calabonga.OperationResults;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calabonga.Catalog2023.Web.Endpoints.TagsEndpoints;
+
+public class TagEndpoints : AppDefinition
+{
+    public override void ConfigureApplication(WebApplication app)
+    {
+        app.MapGet("/api/tags/get-cloud", ReviewGetForCreate);
+
+    }
+
+    [ProducesResponseType(200)]
+    [ProducesResponseType(401)]
+    [FeatureGroupName("Tags")]
+    private Task<OperationResult<IEnumerable<TagCloud>>> ReviewGetForCreate
+    (
+        [FromServices] IMediator mediator,
+        HttpContext context)
+    {
+        return mediator.Send(new GetTagCloudRequest(), context.RequestAborted);
+    }
+}
